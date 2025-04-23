6e8e974b53b42255fd333a70866d9bf5f9e84864
jonasluis <jonasluis66@gmail.com>
2025-04-23 16:22:00 -0300

    feat: cria regra dos negocios/ services

---
+++ src/main/java/desafio/itau/springboot/service/TansactionService.java
@@ -0,0 +1,33 @@
+package desafio.itau.springboot.service;
+
+import desafio.itau.springboot.model.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+@Service
+public class TansactionService {
+
+    private  final Queue<Transaction> transactions = new ConcurrentLinkedDeque<>();
+
+    public  void addTransaction(Transaction transaction){
+        transactions.add(transaction);
+    }
+    public  void clearTransaction(){
+        transactions.clear();
+    }
+
+    public DoubleSummaryStatistics getStatistics() {
+        OffsetDateTime now = OffsetDateTime.now();
+        return  transactions.
+                stream().
+                filter(t -> t.getDataHora().
+                        isAfter(now.minusSeconds(60))).
+                mapToDouble(Transaction::getValor).
+                summaryStatistics();
+
+    }
+}
